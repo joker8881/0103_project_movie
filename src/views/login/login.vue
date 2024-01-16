ae5545c8e4ea48808ae68453ff145accd155d153
joker8881 <joker302876@gmail.com>
2024-01-16 18:02:18 +0800

    註冊頁版面完成，java待接

---
+++ src/views/login/login.vue
@@ -0,0 +1,194 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+export default {
+  data() {
+    return{
+      cBox:false,
+      account:"",
+      password:"",
+      setacc:"",
+      accall:[],
+      acc:[],
+      pas:[],
+      show:0,
+      dog:{
+        date:21,
+        name:"果醬",
+        money:400
+      },
+      cat:{},
+    }
+  },
+  components: {
+    RouterLink,
+  },
+  methods:{
+    log(){
+      if(this.cBox == true){
+        localStorage.setItem("keep","keep")
+        localStorage.setItem("setacc",this.account)
+      }
+      this.acc = JSON.parse(localStorage.getItem("account"))
+      this.pas = JSON.parse(localStorage.getItem("password"))
+      console.log(this.acc)
+      console.log(this.pas)
+      for(let i = 0 ; i<this.acc.length ; i++){
+        if( this.acc[i] == this.account){
+          if(this.pas[i] == this.password){
+            localStorage.setItem("logacc",this.account)
+            this.$router.push("/count")
+          } else{
+            localStorage.removeItem("setacc")
+            break
+          }
+        }
+      }
+    },
+    clickC(){
+      let e = document.getElementsByName("eye")
+      let acc = document.getElementById("acc")
+      if(e.class == "fa-solid fa-eye fa-lg eye"){
+        e.class="fa-solid fa-eye-slash fa-lg eye"
+        acc.type="text"
+        this.show = 1
+      } else{
+        e.class="fa-solid fa-eye fa-lg eye"
+        acc.type="password"
+        this.show = 0
+      }
+    },
+    register(){
+        this.$router.push("/register")
+    }
+  },
+  mounted(){
+    if(localStorage.getItem("keep") == "keep"){
+      this.setacc =localStorage.getItem("setacc")
+      this.acc = JSON.parse(localStorage.getItem("account"))
+      this.pas = JSON.parse(localStorage.getItem("password"))
+      for(let i = 0 ; i<this.acc.length ; i++){
+        if( this.acc[i] == this.setacc){
+            console.log(this.acc[i])
+            console.log(this.pas[i])
+            this.account = this.acc[i]
+            this.password = this.pas[i]
+          }
+        }
+      }
+    localStorage.removeItem("keep")
+    localStorage.removeItem("setacc")
+    localStorage.removeItem("logacc")
+  }
+};
+</script>
+
+<template>
+    <div class="cBox">
+        <div class="box">
+            <p class="textT">記帳小能手</p>
+            <p class="textL">帳號</p>
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control tb" id="floatingInput" placeholder="" v-model="this.account">
+                <label class="tbc" for="floatingInput">請在這裡輸入帳號</label>
+            </div>
+            <p class="textL">密碼</p>
+            <div class="form-floating mb-3">
+                <input type="password" class="form-control tbp" id="acc" placeholder="" v-model="this.password">
+                <i v-if="this.show == 0" class="fa-solid fa-eye fa-lg eye" @click="this.clickC()" name="eye"></i>
+                <i v-if="this.show == 1" class="fa-solid fa-eye-slash fa-lg eye" @click="this.clickC()" name="eye"></i>
+                <label class="tbc" for="floatingInput">請在這裡輸入密碼</label>
+            </div>
+            <div class="checkbox">
+                <input class="leftC" type="checkbox" name="" id="cBox" v-model="cBox">
+                <p class="textC">保留我的登入資訊</p>
+            </div>
+            <div class="logbox">
+                <button type="button" class="button bc" @click="register">註冊帳號</button>
+                <button type="button" class="button" @click="log()">登入</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.cBox{
+  width: 100vw;
+  height: 90vh;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  .box{
+  height: 80%;
+  width: 40%;
+  // margin-top: 2%;
+  align-self: center;
+  align-items: center;
+  background-color: #525f75;
+  border-radius: 15px;
+  .textT{
+    font-family:'jf-openhuninn-2.0';
+    font-size: 2em;
+    margin-top: 20px;
+    color: white;
+  }
+  .textL{
+    font-family:'jf-openhuninn-2.0';
+    font-size: 1.5em;
+    text-align: start;
+    margin-left: 12%;
+    color: white;
+  }
+  .tb{
+    width: 80%;
+    margin: 0 auto;
+  }
+  .tbp{
+    width: 80%;
+    margin: 0 auto;
+    position: relative;
+  }
+  .eye{
+    position: absolute;
+    bottom: 50%;
+    right: 14%;
+    transition: 0.3s;
+    &:hover{
+      color: rgb(255, 173, 65);
+    }
+  }
+  .tbc{
+    font-family:'jf-openhuninn-2.0';
+    font-size: 1em;
+    margin-left: 10%;
+  }
+  .checkbox{
+    align-items: start;
+    display: flex;
+    margin-left: 10%;
+    .leftC{
+    margin-top: 1.5%;
+  }
+  .textC{
+    margin-left: 1.2%;
+    color: white;
+  }
+  }
+  .logbox{
+    margin: 0 auto;
+    display: flex;
+    height: 15%;
+    width: 80%;
+    justify-content: space-between;
+    .button{
+      width: 35%;
+      height: 55%;
+      border: none;
+      background-color: rgb(176, 182, 213);
+      border-radius: 10px;
+      font-size: 1.5em;
+      color: rgb(0, 0, 0);
+    }
+  }
+}
+}
+</style>

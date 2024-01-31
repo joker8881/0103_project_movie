1793bfdfabeb79033a222cd3aa9894b7ac070a0f
通報した <peterjackson0107@gmail.com>
2024-01-31 11:32:28 +0800

    20240131-1132 類型

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,7 +68,7 @@ export default {
     },
   },
   methods: {
-    async getPlayMovie() { // 上映中123
+    async getPlayMovie() { // 上映中
       const options = {
         method: "GET",
         headers: {
